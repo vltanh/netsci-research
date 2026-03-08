82d27d2ecd2f9062f17f5420b98ca51a06a04055
The-Anh Vu-Le <vltanh@illinois.edu>
2026-03-08 18:22:48 -0500

    refactor(plots): organize plotting scripts

--- make_plots.sh
+++ make_plots.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python aggregate_comparisons.py \
-    --output-dir plots/ec-sbm/ \
-    --output-fn agg_comp.csv \
+python network_evaluation/compare/aggregate_comparisons.py \
+    --root data/synthetic_networks/stats/ \
+    --output plots/ec-sbm/agg_comp.csv \
     --network-fp data/networks_verysmall.txt \
     --generators ec-sbm-v2 ec-sbm-v1.5 \
     --clusterings \
@@ -17,7 +17,7 @@ python aggregate_comparisons.py \
         "sbm-flat-best+wcc(log)" \
         "sbm-nested-best+wcc(log)"
 
-python visualize_comparisons.py \
+python network_evaluation/compare/visualize_comparisons.py \
     --data-fp plots/ec-sbm/agg_comp.csv \
     --generators \
         ec-sbm-v2 \
@@ -125,70 +125,3 @@ python visualize_comparisons.py \
     --output-dir plots/ec-sbm/very_small/ \
     --output-fn network \
     --hide-fliers
-
-# python visualize_comparisons.py \
-#     --data-fp plots/ec-sbm/agg_comp.csv \
-#     --generators \
-#         ec-sbm-v2 \
-#         ec-sbm-v2 \
-#         ec-sbm-v2 \
-#         ec-sbm-v2 \
-#         ec-sbm-v2 \
-#         ec-sbm-v2 \
-#     --clusterings \
-#         leiden-cpm-0.5 \
-#         leiden-cpm-0.5+cm \
-#         leiden-cpm-0.1 \
-#         leiden-cpm-0.1+cm \
-#         leiden-mod \
-#         leiden-mod+cm \
-#     --names \
-#         "EC-SBMv2 x Leiden-CPM(0.5)" \
-#         "EC-SBMv2 x Leiden-CPM(0.5)+CM" \
-#         "EC-SBMv2 x Leiden-CPM(0.1)" \
-#         "EC-SBMv2 x Leiden-CPM(0.1)+CM" \
-#         "EC-SBMv2 x Leiden-Mod" \
-#         "EC-SBMv2 x Leiden-Mod+CM" \
-#     --stats \
-#         n_edges \
-#         deg_assort \
-#         global_ccoeff \
-#         local_ccoeff \
-#         pseudo_diameter \
-#         char_time \
-#         node_percolation_targeted \
-#         node_percolation_random \
-#         degree \
-#         local_ccoeff_nodes \
-#         pagerank \
-#         kcore \
-#     --types \
-#         scalar \
-#         scalar \
-#         scalar \
-#         scalar \
-#         scalar \
-#         scalar \
-#         scalar \
-#         scalar \
-#         sequence \
-#         sequence \
-#         sequence \
-#         sequence \
-#     --metrics \
-#         rel_diff \
-#         abs_diff \
-#         abs_diff \
-#         abs_diff \
-#         rel_diff \
-#         abs_diff \
-#         abs_diff \
-#         abs_diff \
-#         rmse \
-#         rmse \
-#         rmse \
-#         rmse \
-#     --network-fp data/networks_train.txt \
-#     --output-dir plots/ec-sbm/v2/train/ \
-#     --output-fn network \
-#     --hide-fliers
