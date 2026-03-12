48678416af4368d2e7fea44873d65cefc4080adf
The-Anh Vu-Le <vltanh@illinois.edu>
2026-03-12 17:55:59 -0500

    feat: add hybrid mode: configuration model followed by greedy

--- make_plots.sh
+++ make_plots.sh
@@ -125,3 +125,97 @@ python network_evaluation/compare/visualize_comparisons.py \
     --output-dir plots/ec-sbm/very_small/ \
     --output-fn network \
     --hide-fliers
+
+python network_evaluation/compare/visualize_comparisons.py \
+    --data-fp plots/ec-sbm/agg_comp.csv \
+    --generators \
+        ec-sbm-v2 \
+        ec-sbm-v1.5 \
+        ec-sbm-v2 \
+        ec-sbm-v1.5 \
+        ec-sbm-v2 \
+        ec-sbm-v1.5 \
+        ec-sbm-v2 \
+        ec-sbm-v1.5 \
+        ec-sbm-v2 \
+        ec-sbm-v1.5 \
+        ec-sbm-v2 \
+        ec-sbm-v1.5 \
+        ec-sbm-v2 \
+        ec-sbm-v1.5 \
+        ec-sbm-v2 \
+        ec-sbm-v1.5 \
+        ec-sbm-v2 \
+        ec-sbm-v1.5 \
+        ec-sbm-v2 \
+        ec-sbm-v1.5 \
+    --clusterings \
+        leiden-cpm-0.5 \
+        leiden-cpm-0.5 \
+        leiden-cpm-0.5+cm \
+        leiden-cpm-0.5+cm \
+        leiden-cpm-0.1 \
+        leiden-cpm-0.1 \
+        leiden-cpm-0.1+cm \
+        leiden-cpm-0.1+cm \
+        leiden-mod \
+        leiden-mod \
+        leiden-mod+cm \
+        leiden-mod+cm \
+        "sbm-flat-best+wcc(sqrt)" \
+        "sbm-flat-best+wcc(sqrt)" \
+        "sbm-flat-best+wcc(log)" \
+        "sbm-flat-best+wcc(log)" \
+        "sbm-nested-best+wcc(sqrt)" \
+        "sbm-nested-best+wcc(sqrt)" \
+        "sbm-nested-best+wcc(log)" \
+        "sbm-nested-best+wcc(log)" \
+    --names \
+        "EC-SBMv2 x Leiden-CPM(0.5)" \
+        "EC-SBMv1.5 x Leiden-CPM(0.5)" \
+        "EC-SBMv2 x Leiden-CPM(0.5)+CM" \
+        "EC-SBMv1.5 x Leiden-CPM(0.5)+CM" \
+        "EC-SBMv2 x Leiden-CPM(0.1)" \
+        "EC-SBMv1.5 x Leiden-CPM(0.1)" \
+        "EC-SBMv2 x Leiden-CPM(0.1)+CM" \
+        "EC-SBMv1.5 x Leiden-CPM(0.1)+CM" \
+        "EC-SBMv2 x Leiden-Mod" \
+        "EC-SBMv1.5 x Leiden-Mod" \
+        "EC-SBMv2 x Leiden-Mod+CM" \
+        "EC-SBMv1.5 x Leiden-Mod+CM" \
+        "EC-SBMv2 x SBM(Flat)+WCC(sqrt)" \
+        "EC-SBMv1.5 x SBM(Flat)+WCC(sqrt)" \
+        "EC-SBMv2 x SBM(Flat)+WCC(log)" \
+        "EC-SBMv1.5 x SBM(Flat)+WCC(log)" \
+        "EC-SBMv2 x SBM(Nested)+WCC(sqrt)" \
+        "EC-SBMv1.5 x SBM(Nested)+WCC(sqrt)" \
+        "EC-SBMv2 x SBM(Nested)+WCC(log)" \
+        "EC-SBMv1.5 x SBM(Nested)+WCC(log)" \
+    --stats \
+        m \
+        c \
+        conductance \
+        degree_density \
+        edge_density \
+        modularity \
+        mincut \
+    --types \
+        sequence \
+        sequence \
+        sequence \
+        sequence \
+        sequence \
+        sequence \
+        sequence \
+    --metrics \
+        rmse \
+        rmse \
+        rmse \
+        rmse \
+        rmse \
+        rmse \
+        rmse \
+    --network-fp data/networks_verysmall.txt \
+    --output-dir plots/ec-sbm/very_small/ \
+    --output-fn cluster \
+    --hide-fliers
